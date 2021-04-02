32960c192b303c733e42345cf7d9700d7b86682e
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-02 20:18:07 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,174 @@
 # react-store-adapter
- Connect stores and mutable sources to React the correct way
+
+> Connect stores and mutable sources to React the correct way.
+
+## Install
+
+```bash
+yarn install react-store-adapter
+```
+
+## Usage
+
+```tsx
+import {
+  StoreAdapterRoot,
+  createStoreAdapter,
+  useStoreAdapter
+} from 'react-store-adapter';
+
+// An example of a non-React global state
+const store = Redux.createStore((state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+});
+
+// Create our store adapter
+const storeAdapter = createStoreAdapter({
+  // How to read the store's state
+  read: () => store.getState(),
+
+  // how to subscribe to the store
+  subscribe: (callback) => store.subscribe(callback),
+});
+
+// Simple reading
+function Count() {
+  // Read our state
+  const count = useStoreAdapter(storeAdapter);
+
+  return (
+    <h1>Count: {count}</h1>
+  );
+}
+
+// Transformed reading
+function getSnapshot(count) {
+  return `Count: ${count}`;
+}
+
+function TransformedCount() {
+  const message = useStoreAdapter(storeAdapter, {
+    getSnapshot,
+  });
+
+  return <h1>{message}</h1>;
+}
+
+// With state memoization
+function shouldUpdate(value) {
+  return value > 10;
+}
+
+function MemoizedCount() {
+  // Will only start re-rendering after count > 10
+  const count = useStoreAdapter(storeAdapter, {
+    shouldUpdate,
+  });
+
+  return <h1>Count: {count}</h1>;
+}
+
+// Render our app
+<StoreAdapterRoot>
+  <Count />
+  <TransformCount />
+  <MemoizedCount />
+</StoreAdapterRoot>
+```
+
+## Features
+
+### State-agnostic Store Adapters
+
+`react-store-adapter` to provide a unified way for React to interact with different kinds of external states and mutable sources (much like what React's Mutable Source's future goal is.)
+
+`createStoreAdapter(options)` requires two fields:
+
+- `read(): State`: Provides a way for the store adapter to access the current state of the store.
+- `subscribe(callback: Function): Function | undefined`: Provides a way for the store adapter to subscribe to the store and receive further updates. It accepts a callback value that is subscribed to the store and must return a cleanup callback which is used to unsubscribe from the store.
+- `shouldUpdate(prev: State, next: State): boolean`: Optional. Tells when the store adapter should update the tracked state. This is useful for emulating immutable states equality (deep comparison).
+
+`createStoreAdapter` then returns a store adapter instance which is used in the React environment (specifically, `useStoreAdapter`).
+
+### Hook
+
+`useStoreAdapter(storeAdapter)` is a hook that provides a way for React components to consume states and updates from the external sources. `useStoreAdapter` accepts a store adapter instance as the first parameter and returns the safe state of the external store. It may also optionally accept an options object as the second parameter:
+
+- `getSnapshot(currentState: State): NewState`: provides a way to transform the safe state. It receives the safe state and must return a new state derived from the safe state.
+- `shouldUpdate(prev: State, next: State): boolean`: tells when should the component update.
+
+### Root
+
+`<StoreAdapterRoot>` is the core of `react-store-adapter`. It handles the communication between your React app and the mutable sources and also keeps track of safe states. The component must always be found in the root of your React app. If there are multiple instances, `react-store-adapter` attemps to only implement a single instance per React tree.
+
+### Safe-state tracking
+
+`react-store-adapter` internally keeps track of which state provided by the external source is safe for the UI components to use. This is to prevent UI tearing where components receive different values unexpectedly.
+
+### UI correctness
+
+`react-store-adapter` detects whenever there's a potential tear in the component, allowing for the component to re-render again with the potential safe state.
+
+### Dispatch correctness
+
+`react-store-adapter` attempts to correct when stores are dispatched. Different timings and schedules of store dispatches may affect how React apps work, which in turn can cause unexpected UI-related issues.
+
+### Selective state updates
+
+`react-store-adapter` provides a way to transform and filter state, which adds a benefit for the UI to save render time specially if the state is considered equal.
+
+## Examples
+
+### Redux
+
+```tsx
+import { createStore } from 'redux';
+
+// An example of a non-React global state
+const store = createStore((state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+});
+
+// Create our store adapter
+const storeAdapter = createStoreAdapter({
+  // How to read the store's state
+  read: () => store.getState(),
+
+  // how to subscribe to the store
+  subscribe: (callback) => store.subscribe(callback),
+});
+```
+
+## Comparison with React's Mutable Source
+
+`react-store-adapter` is inspired by the implementation of the proposed [useMutableSource](https://github.com/reactjs/rfcs/blob/master/text/0147-use-mutable-source.md) with minor differences.
+
+- `createStoreAdapter` and `createMutableSource` provides different interfaces.
+- `react-store-adapter` allows selective state updates while React's Mutable Source keeps track of state versions to signify state changes.
+- `react-store-adapter` works in React 16.8.0 and above, while React's Mutable Source is still in development.
+- `react-store-adapter` requires the `<StoreAdapterRoot>` to be implemented. React's Mutable Source doesn't have any as it lives within the VDOM.
+- `react-store-adapter` does not correct it's UI during render phase, it does so during passive effects. React's Mutable Source does so when it detects state changes during render phase.
+- `react-store-adapter` defers state updates of the wrapped store to the passive effects. This allows store updates to be scheduled at the same phase as passive effects and to not interupt the render phase.
+
+There are also some similarities:
+
+- Both keeps tracks of the safe state. Safe state is a kind of state that is safe for the UI to present. This prevents the UI from tearing during render phase whenever the store updates untimely.
+- Both achieves UI correctness, just at different times.
+
+## License
+
+MIT © [Lyon Software Technologies, Inc.](https://github.com/LyonInc)
---
+++ src/StoreAdapter.tsx
@@ -0,0 +1,414 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2021 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2021
+ */
+import {
+  Subscription,
+  useConstant,
+  useConstantCallback,
+  useMemoCondition,
+  useSubscription,
+} from '@lyonph/react-hooks';
+import React, {
+  createContext,
+  FC,
+  memo,
+  MutableRefObject,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+type NotifierListener<T> = (value: T) => void;
+
+export default class Notifier<T> {
+  private alive = true;
+
+  private listeners = new Set<NotifierListener<T>>();
+
+  private value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  public subscribe(listener: NotifierListener<T>): () => void {
+    if (this.alive) {
+      this.listeners.add(listener);
+    }
+
+    return () => {
+      if (this.alive) {
+        this.listeners.delete(listener);
+      }
+    };
+  }
+
+  public notify(value: T): void {
+    if (this.alive) {
+      this.value = value;
+      this.listeners.forEach((listener) => {
+        listener(value);
+      });
+    }
+  }
+
+  public destroy(): void {
+    if (this.alive) {
+      this.listeners.clear();
+      this.alive = false;
+    }
+  }
+
+  public read(): T {
+    return this.value;
+  }
+
+  public hasListeners(): boolean {
+    return this.listeners.size > 0;
+  }
+}
+
+type Subscribe = (callback: () => void) => Unsubscribe;
+type Unsubscribe = undefined | (() => void) | void;
+
+interface StoreAdapterBase<T> {
+  readonly read: () => T;
+  readonly subscribe: Subscribe;
+}
+
+interface StoreAdapterOptions<T> extends StoreAdapterBase<T> {
+  readonly id?: string;
+  readonly shouldUpdate?: (prev: T, next: T) => boolean;
+}
+
+export interface StoreAdapter<T> extends StoreAdapterBase<T> {
+  readonly id: string;
+  readonly shouldUpdate: (prev: T, next: T) => boolean;
+}
+
+let index = 0;
+
+function getIndex() {
+  const current = index;
+  index += 1;
+  return current;
+}
+
+function defaultUpdate<T>(prev: T, next: T): boolean {
+  return !Object.is(prev, next);
+}
+
+export function createStoreAdapter<T>(
+  options: StoreAdapterOptions<T>,
+): StoreAdapter<T> {
+  return {
+    id: `Store-${getIndex()}`,
+    shouldUpdate: defaultUpdate,
+    ...options,
+  };
+}
+
+interface StoreAdapterContext {
+  register<T>(store: StoreAdapter<T>): void;
+  read<T>(store: StoreAdapter<T>): T;
+  subscribe<T>(store: StoreAdapter<T>, callback: () => void): () => void;
+}
+
+type StoreAdapterContextRef = MutableRefObject<StoreAdapterContext | undefined>;
+
+const StoreAdapterContext = (
+  createContext<StoreAdapterContextRef | undefined>(undefined)
+);
+
+interface StoreAdapterMemory<T> {
+  notifier: Notifier<T>;
+  unsubscribe?: () => void;
+}
+
+function useStoreAdapterContextRef(): StoreAdapterContextRef {
+  const context = useContext(StoreAdapterContext);
+
+  if (context) {
+    return context;
+  }
+
+  throw new Error('Attempt to access missing StoreAdapterContext reference.');
+}
+
+function useStoreAdapterContext(): StoreAdapterContext {
+  const context = useStoreAdapterContextRef();
+
+  if (context.current) {
+    return context.current;
+  }
+
+  throw new Error('Attempt to access missing StoreAdapterContext.');
+}
+
+const StoreAdapterCore = memo(() => {
+  const isMounted = useRef(true);
+
+  useEffect(() => () => {
+    isMounted.current = false;
+  }, []);
+
+  const memory = useConstant(() => new Map<string, StoreAdapterMemory<any>>());
+
+  const pendingStores = useRef<StoreAdapter<any>[]>([]);
+  const [pendingStoresVersion, setPendingStoresVersion] = useState([]);
+
+  const pendingUpdates = useRef<(() => void)[]>([]);
+  const [pendingUpdatesVersion, setPendingUpdatesVersion] = useState([]);
+
+  const batchUpdates = useConstantCallback((callback: () => void) => {
+    if (isMounted.current) {
+      pendingUpdates.current.push(callback);
+      setPendingUpdatesVersion([]);
+    }
+  });
+
+  // This effect runs all the pending store registrations.
+  useEffect(() => {
+    const batch = pendingStores.current;
+    if (batch.length) {
+      // Clear the current pending list
+      pendingStores.current = [];
+
+      // Iterate the pending list
+      batch.forEach((store) => {
+        // Check instance
+        const instance = memory.get(store.id);
+        if (instance) {
+          const checkForUpdates = () => {
+            // Only subscribe if StoreAdapter root
+            if (isMounted.current) {
+              // Read our next value
+              const nextValue = store.read();
+
+              if (store.shouldUpdate(instance.notifier.read(), nextValue)) {
+                // If the store was dispatched during render phase
+                // and the store notification is synchronous
+                // to the dispatch call, we defer the proxy update
+                setTimeout(() => {
+                  // Update has been deferred, but the timing is off to the
+                  // React cycle. We defer the update again to the passive
+                  // effects.
+                  batchUpdates(() => {
+                    instance.notifier.notify(nextValue);
+                  });
+                });
+              }
+            }
+          };
+
+          // Subscribe to the store
+          const unsubscribe = store.subscribe(checkForUpdates);
+
+          // Check for updates immediately
+          // to know if the store has changed
+          // states
+          checkForUpdates();
+
+          // If there's a subscription, we set it for later cleanup
+          if (unsubscribe) {
+            instance.unsubscribe = unsubscribe;
+          }
+        }
+      });
+    }
+  }, [batchUpdates, memory, pendingStoresVersion]);
+
+  // This effect is for running all scheduled updates
+  // from proxy stores
+  useEffect(() => {
+    const batch = pendingUpdates.current;
+    if (batch.length) {
+      pendingUpdates.current = [];
+
+      batch.forEach((update) => {
+        if (isMounted.current) {
+          update();
+        }
+      });
+    }
+  }, [pendingUpdatesVersion]);
+
+  // This effect performs cleanup on unmount
+  useEffect(() => () => {
+    memory.forEach((item) => {
+      if (item.unsubscribe) {
+        item.unsubscribe();
+      }
+      item.notifier.destroy();
+    });
+    memory.clear();
+  }, [memory]);
+
+  const contextRef = useStoreAdapterContextRef();
+
+  if (!contextRef.current) {
+    const getInstance = <T, >(store: StoreAdapter<T>): StoreAdapterMemory<T> => {
+      // Get instance
+      let instance = memory.get(store.id) as StoreAdapterMemory<T>;
+      if (!instance) {
+        // Create instance if nothing is found
+        const proxy = new Notifier<T>(store.read());
+        instance = {
+          notifier: proxy,
+        };
+
+        // If root is still mounted, write to memory
+        if (isMounted.current) {
+          memory.set(store.id, instance);
+        }
+      }
+      return instance;
+    };
+    contextRef.current = {
+      register: (store) => {
+        if (isMounted.current) {
+          pendingStores.current.push(store);
+          setPendingStoresVersion([]);
+        }
+      },
+      read: (store) => getInstance(store).notifier.read(),
+      subscribe: (store, callback) => {
+        const instance = getInstance(store);
+
+        const unsubscribe = instance.notifier.subscribe(callback);
+
+        return () => {
+          // Attempt to unsubscribe
+          unsubscribe();
+
+          // Self-destroy instance of there are no listeners
+          if (!instance.notifier.hasListeners()) {
+            if (instance.unsubscribe) {
+              instance.unsubscribe();
+            }
+            instance.notifier.destroy();
+
+            memory.delete(store.id);
+          }
+        };
+      },
+    };
+  }
+
+  return null;
+}, () => true);
+
+function identity<T, R>(value: T): R {
+  return value as unknown as R;
+}
+
+interface UseStoreAdapterOptions<T, R> {
+  getSnapshot?: (value: T) => R;
+  shouldUpdate?: (prev: R, next: R) => boolean;
+}
+
+export function useStoreAdapter<T>(
+  store: StoreAdapter<T>,
+): T;
+export function useStoreAdapter<T, R>(
+  store: StoreAdapter<T>,
+  options: UseStoreAdapterOptions<T, R>,
+): R;
+export function useStoreAdapter<T, R>(
+  store: StoreAdapter<T>,
+  options?: UseStoreAdapterOptions<T, R>,
+): R {
+  // Apply default values
+  const getSnapshot = options?.getSnapshot ?? identity;
+  const shouldUpdate = options?.shouldUpdate ?? defaultUpdate;
+
+  // Access adapter root context
+  const context = useStoreAdapterContext();
+
+  useEffect(() => {
+    context.register(store);
+  }, [store, context]);
+
+  // Create subscription
+  const subscription = useMemoCondition(
+    (): Subscription<R> => ({
+      read: () => getSnapshot(context.read(store)),
+      subscribe: (callback) => context.subscribe(store, callback),
+      shouldUpdate,
+    }),
+    {
+      store,
+      getSnapshot,
+      shouldUpdate,
+    },
+    (prev, next) => (
+      !Object.is(prev.store, next.store)
+      || !Object.is(prev.getSnapshot, next.getSnapshot)
+      || !Object.is(prev.shouldUpdate, next.shouldUpdate)
+    ),
+  );
+
+  const state = useSubscription(subscription);
+
+  useDebugValue(state);
+
+  return state;
+}
+
+const StoreAdapterRootInternal: FC = ({ children }) => {
+  const ref = useRef<StoreAdapterContext>();
+  return (
+    <StoreAdapterContext.Provider value={ref}>
+      <StoreAdapterCore />
+      {children}
+    </StoreAdapterContext.Provider>
+  );
+};
+
+export const StoreAdapterRoot: FC = ({ children }) => {
+  const context = useContext(StoreAdapterContext);
+
+  // There only should be a single root for every tree
+  if (context) {
+    return <>{children}</>;
+  }
+
+  return (
+    <StoreAdapterRootInternal>
+      {children}
+    </StoreAdapterRootInternal>
+  );
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  StoreAdapterContext.displayName = 'StoreAdapterContext';
+  StoreAdapterCore.displayName = 'StoreAdapterCore';
+  StoreAdapterRootInternal.displayName = 'StoreAdapterRootInternal';
+  StoreAdapterRoot.displayName = 'StoreAdapterRoot';
+}
